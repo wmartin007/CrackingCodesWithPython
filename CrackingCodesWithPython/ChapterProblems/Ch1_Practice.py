cf58c6ea0a20eb59701294c8ddac3093ef1bc261
Unknown <wmartin007@gmail.com>
2018-05-03 15:20:34 -0400

    Chapter 1 update - 20180503

--- CrackingCodesWithPython/ChapterProblems/Ch1_Practice.py
+++ CrackingCodesWithPython/ChapterProblems/Ch1_Practice.py
@@ -1,5 +1,38 @@
 from string import ascii_lowercase
 
-x = ascii_lowercase
+lower_alpha = ascii_lowercase
+i_letter = dict(enumerate(lower_alpha))
+caesar = dict((v,k) for k,v in i_letter.items())
 
-print(list(x)[1])
+test_string = "AMBIDEXTROUS: Able to pick with equal skill a right hand pocket or a left."
+
+
+
+def caesar_num(letter, key):
+    x = caesar[letter] + key
+    if x > 25:
+        x = 26 - x
+    return i_letter[x]
+
+for a in test_string:
+    if a.isalpha() and a.islower():
+        print(caesar_num(a, 4))
+    else:
+        print(a)
+
+
+def encrypt(input_str, key):
+    encrypted_str = []
+    for c in input_str:
+        if c.isalpha():
+            if c.isupper():
+                encrypted_str.append(i_letter[caesar_num(c.lower,key)].upper())
+            else:
+                encrypted_str.append(i_letter[caesar_num(c,key)])
+        else:
+            encrypted_str.append(c)
+    return encrypted_str
+
+#test = encrypt(test_string,4)
+
+#print(test)
